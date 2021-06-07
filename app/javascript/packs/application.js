b361c5b7724608b22ee1d2b2dc33cd2a50ebbbb6
ricardosawir <sawir.ricardo@gmail.com>
2021-06-07 16:19:56 +0700

    added analytics

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+$(document).on('turbolinks:load', function () {
+	gtag('config', 'G-8195TYYBVP', { page_location: event.data.url });
+});
